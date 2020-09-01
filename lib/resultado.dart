a1a56510b1916e2c87904d8e58d5dd02ec0b35b8
maurozem <maurozem@brq.com>
2020-09-01 12:14:30 -0300

    finalização do projeto

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'parabéns';
+    } else if (pontuacao < 12) {
+      return 'você é bom';
+    } else if (pontuacao < 16) {
+      return 'impressionante';
+    } else {
+      return 'nível jedi';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Parabéns',
-        style: TextStyle(fontSize: 28),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        FlatButton(
+          onPressed: quandoReiniciarQuestionario,
+          child: Text(
+            'Reiniciar?',
+            style: TextStyle(fontSize: 18),
+          ),
+          textColor: Colors.blue,
+        )
+      ],
     );
   }
 }
